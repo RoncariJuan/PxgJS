7238e28152d410ba93d3992dd2ebad67c429e0f4
JuanRoncari <kiritoo10@outlook.com>
2025-03-03 09:20:25 -0300

    alteração do Ingles

--- javascript/modules/eng.js
+++ javascript/modules/eng.js
@@ -3,20 +3,19 @@ export default function English(){
     const english2 = document.querySelector('[data-bolinha-eng2]');
     const english3 = document.querySelector('[data-bolinha-eng3]');
     const sociais = document.querySelector('.sociais');
-    sociais.classList.add('.sociais');
+    
     english1.addEventListener('click', () => {
-        const eng = document.querySelector('[data-span-eng]');  
-        eng.textContent = `There are over 400 creatures!`
-    });
-
-    english2.addEventListener('click', () => {
-        const eng = document.querySelector('[data-span-eng]');  
-        eng.textContent = `Travel with your creatures!`
-    });
-
-    english3.addEventListener('click', () => {
-        const eng = document.querySelector('[data-span-eng]');  
-        eng.textContent = `Fight in real-time!`
+       const span = document.querySelector('[data-span-eng]');
+       span.textContent = 'There are over 400 creatures!';
     });
     
+    english2.addEventListener('click', () => {
+        const span = document.querySelector('[data-span-eng]');
+        span.textContent = 'Travel with your creatures!';
+     });
+
+    english3.addEventListener('click', () => {
+        const span = document.querySelector('[data-span-eng]');
+        span.textContent = 'Fight in real-time!';
+    });
 }
